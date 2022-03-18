537acca7917fccfcf956f1241e566e968217246f
Nex <nex@bits.team>
2022-03-18 16:24:20 +0100

    Update to 1.18.2 (#18)
    Update to 1.18.2
    
    Bump to Loom 0.12
    
    Update to 1.18.2
    
    Reviewed-on: https://hogwarts.bits.team/git/Bits/Nibbles/pulls/18
    Reviewed-by: Kallum <kallummj071001@gmail.com>

--- src/main/java/team/bits/nibbles/mixin/ServerInstanceMixin.java
+++ src/main/java/team/bits/nibbles/mixin/ServerInstanceMixin.java
@@ -1,17 +1,6 @@
 package team.bits.nibbles.mixin;
 
-import com.mojang.authlib.GameProfileRepository;
-import com.mojang.authlib.minecraft.MinecraftSessionService;
-import com.mojang.datafixers.DataFixer;
-import net.minecraft.resource.ResourcePackManager;
-import net.minecraft.resource.ServerResourceManager;
-import net.minecraft.server.WorldGenerationProgressListenerFactory;
 import net.minecraft.server.dedicated.MinecraftDedicatedServer;
-import net.minecraft.server.dedicated.ServerPropertiesLoader;
-import net.minecraft.util.UserCache;
-import net.minecraft.util.registry.DynamicRegistryManager;
-import net.minecraft.world.SaveProperties;
-import net.minecraft.world.level.storage.LevelStorage;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -22,15 +11,10 @@ import team.bits.nibbles.utils.ServerInstance;
 public class ServerInstanceMixin {
 
     @Inject(
-            method = "<init>(Ljava/lang/Thread;Lnet/minecraft/util/registry/DynamicRegistryManager$Impl;Lnet/minecraft/world/level/storage/LevelStorage$Session;Lnet/minecraft/resource/ResourcePackManager;Lnet/minecraft/resource/ServerResourceManager;Lnet/minecraft/world/SaveProperties;Lnet/minecraft/server/dedicated/ServerPropertiesLoader;Lcom/mojang/datafixers/DataFixer;Lcom/mojang/authlib/minecraft/MinecraftSessionService;Lcom/mojang/authlib/GameProfileRepository;Lnet/minecraft/util/UserCache;Lnet/minecraft/server/WorldGenerationProgressListenerFactory;)V",
+            method = "<init>",
             at = @At("TAIL")
     )
-    public void onServerConstruct(
-            Thread serverThread, DynamicRegistryManager.Impl registryManager, LevelStorage.Session session,
-            ResourcePackManager dataPackManager, ServerResourceManager serverResourceManager,
-            SaveProperties saveProperties, ServerPropertiesLoader propertiesLoader, DataFixer dataFixer,
-            MinecraftSessionService sessionService, GameProfileRepository gameProfileRepo, UserCache userCache,
-            WorldGenerationProgressListenerFactory worldGenerationProgressListenerFactory, CallbackInfo ci) {
+    public void onServerConstruct(CallbackInfo ci) {
 
         ServerInstance.set((MinecraftDedicatedServer) (Object) this);
     }
