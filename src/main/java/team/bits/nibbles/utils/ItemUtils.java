d26f92d39ffab9d164756657752a3185ae08469c
Kallum <kallummj071001@gmail.com>
2022-07-19 13:09:37 +0200

    Add method to transform itemstacks into command compatible strings (#23)
    Add method to transform itemstacks into command compatible strings
    
    Co-authored-by: Kallum Jones <kallummj071001@gmail.com>
    Reviewed-on: https://hogwarts.bits.team/git/Bits/Nibbles/pulls/23
    Reviewed-by: Nex <nex@bits.team>

--- src/main/java/team/bits/nibbles/utils/ItemUtils.java
+++ src/main/java/team/bits/nibbles/utils/ItemUtils.java
@@ -2,6 +2,7 @@ package team.bits.nibbles.utils;
 
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtHelper;
 import net.minecraft.nbt.NbtList;
 import net.minecraft.nbt.NbtString;
 import net.minecraft.text.Text;
@@ -21,5 +22,24 @@ public class ItemUtils {
         displayNbt.put(ItemStack.LORE_KEY, loreNbt);
 
         stack.setSubNbt(ItemStack.DISPLAY_KEY, displayNbt);
+    }
+
+    /**
+     * Returns string that can be used in commands relating to this item stack, e.g /give
+     * @param stack the ItemStack to transform into a string
+     * @return the generated string
+     */
+    public static String stackToCommandString(ItemStack stack) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(stack.getItem());
+
+        if (stack.getNbt() != null) {
+            String nbt = NbtHelper.toNbtProviderString(stack.getNbt()).trim();
+            // Remove whitespace outside of quotes
+            nbt = nbt.replaceAll("\\s+(?=([^\"]*\"[^\"]*\")*[^\"]*$)","");
+            builder.append(nbt);
+        }
+
+        return builder.toString();
     }
 }
